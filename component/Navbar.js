6e37723d435aaadd5586936bdfc3143ada6ed40e
kevin <kevin.saffioti@gmail.com>
2021-09-13 09:39:07 +0200

    add navbar

---
+++ component/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function Navbar (){
+    return(
+        <div className="nav">
+            <div className="nav-title">
+                <ul>
+                    <li>Présentation</li>
+                    <li>Actualités</li>
+                    <li>Qu'est-ce que c'est ?</li>
+                    <li>Pour qui ?</li>
+                    <li>Quand consulter</li>
+                    <li>Accompagnement</li>
+                    <li>Localisation</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
