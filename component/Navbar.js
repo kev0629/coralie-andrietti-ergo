fb9724410eac679a23ab765dc0eb8fd7143ed2eb
kevin <kevin.saffioti@gmail.com>
2021-09-14 09:18:28 +0200

    add logo in navbar

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Image from "next/image"
+
+import logo_title from "../public/pictures/Logo_title.svg"
 
 export default function Navbar (){
     return(
         <div className="nav">
-            <div className="nav-titles">
-                <ul>
+            <Image src={logo_title} className="logo-nav" width={170} height={170} alt='Coralie Andrietti logo'/>
+                <ul className="nav-titles">
                     <li className="nav-title">Présentation</li>
                     <li className="nav-title">Actualités</li>
                     <li className="nav-title">Qu'est-ce que c'est ?</li>
@@ -13,7 +16,6 @@ export default function Navbar (){
                     <li className="nav-title">Accompagnement</li>
                     <li className="nav-title">Localisation</li>
                 </ul>
-            </div>
         </div>
     )
 }
