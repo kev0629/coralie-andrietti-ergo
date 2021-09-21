beb947f6afdcf7dfd43df7cabcdcd24296714b39
kevin <kevin.saffioti@gmail.com>
2021-09-21 08:24:38 +0200

    Update Presentation.js

--- component/Presentation.js
+++ component/Presentation.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
-export default function Home(){
+export default function Presentation(){
     return(
         <div>
-            
+            <Image src='/../public/pictures/0.jpeg'alt='coralie Andrietti' width={175} height={203}/>
+            <Image src='/../public/pictures/Coralie_ergo_tittle.svg'alt='title coralie ergo' width={200} height={170}/>
         </div>
     )
 }
