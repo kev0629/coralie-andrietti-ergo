7b0b7d593fcd101bb660fef98e4f39a2c17ab0b3
Kevin Saffioti <kevin.saffioti@gmail.com>
2021-11-10 10:40:32 +0100

    add link to gmap and add blur for image in following component

--- component/Localisation.js
+++ component/Localisation.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import { Link } from "next/link";
+import Link from "next/link";
 import cabinet_picture from '../public/pictures/office_pic.png'
 import map from '../public/pictures/map.png'
 
 export default function Localisation(){
-
+    const gmapLink = "https://www.google.com/maps/place/Coralie+ANDRIETTI+Ergoth%C3%A9rapeute/data=!3m1!4b1!4m2!3m1!1s0x12cdd58e043b1ed1:0x51932fb1d3a7312e"
     return(
         <>  
 
@@ -19,21 +19,33 @@ export default function Localisation(){
                 </div>
                 <div className='centered-content'>
                     <p className='text-size-mid'>Le cabinet se situe au :</p>
-                    {/* <Link href={gmapLink} passHref={true}> */}
-                    <p className='text-size-high'>Résidence l'Estérel entrée A</p>
+                    {/* <Link href="https://www.google.com/maps/place/Coralie+ANDRIETTI+Ergoth%C3%A9rapeute/data=!3m1!4b1!4m2!3m1!1s0x12cdd58e043b1ed1:0x51932fb1d3a7312e" passHref={true}> */}
+                    <Link href={gmapLink} passHref={true}>
+                        <p className='text-size-high'>Résidence l'Estérel entrée A</p>
+                    </Link>
+                    <Link href={gmapLink} passHref={true}>
                     <p className='text-size-high'>100 chemin de l'Olivet</p>
+                    </Link>
+                    <Link href={gmapLink} passHref={true}>
                     <p className='text-size-high'>06110 le Cannet</p>
+                    </Link>
+                    <Link href={gmapLink} passHref={true}>
                     <p className='text-size-high'>France</p>
+                    </Link>
                     {/* </Link> */}
                     <p>Petit coup de pouce : l'entrée se situe entre l'auto-école Colette et le salon de coiffure " le salon d'Élodie"</p>
                 </div>
             </div>
+            
             <div className='centered-content'>
-                <Image src={map} 
-                       alt='map'
-                       width={2800}
-                       height={1000}
-                       placeholder='blur'/>
+                <Link href={gmapLink} passHref={true}>
+                    <Image src={map} 
+                        alt='map'
+                        width={2800}
+                        height={1000}
+                        placeholder='blur'/>
+                </Link>
+                
             </div>
             
             
