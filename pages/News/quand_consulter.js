685cce30791f0b77e0aae8dedd6575b911ab0f39
Kevin Saffioti <kevin.saffioti@gmail.com>
2021-11-19 18:47:08 +0100

    articles text completed

--- pages/News/quand_consulter.js
+++ pages/News/quand_consulter.js
@@ -8,7 +8,12 @@ export default function QuandConsulter() {
     return (
         <Body>
             <Section title={articles[articleIndex].title}>
-            <div dangerouslySetInnerHTML={{__html: articles[articleIndex].body}}></div>
+                Ce sont sûrement les questions les plus fréquentes qui me sont posées lorsque je dois intervenir dans les écoles.<br/>
+                Quasiment à chacune de mes interventions, un enseignant m’aborde pour me parler d’un élève qui lui paraît avoir des difficultés.<br/>
+                Systématiquement, l’enseignant me demande mon avis sur la pertinence d’envoyer cet enfant consulter un ergothérapeute.<br/>
+                <br/>
+                Merci aux ergothérapeutes qui sont à l’origine de ces fiches pour aider les enseignants en milieu scolaire. 
+                J’espère que leur diffusion permettra une action précoce auprès des bambins, enfants et adolescents que nous suivons dans leur cursus scolaire.<br/>
             </Section>
         </Body>
     )}
