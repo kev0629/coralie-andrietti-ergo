c6dd602185b8b6a4dc613b54fb83e6baed5d59f6
Kevin Saffioti <kevin.saffioti@gmail.com>
2022-01-07 11:09:38 +0100

    add arrow in article

--- pages/News/Les_troubles_neuro_visuels.js
+++ pages/News/Les_troubles_neuro_visuels.js
@@ -2,6 +2,7 @@ import Body from '../../component/Body'
 import Section from '../../component/Section'
 import articles from '../api/articles'
 import Image from 'next/image'
+import arrow from '../../public/pictures/arrow-right.svg'
 
 import React from 'react'
 
@@ -33,48 +34,52 @@ export default function Les_troubles_neuro_visuels() {
             Les saccades permettent de regarder d’un objet à un autre rapidement. 
             La poursuite oculaire permet de suivre du regard un objet en mouvement.<br/>
             <br/> 
-            -> En bilan, l’ergothérapeute va, par exemple, vérifier que l’enfant (selon l’âge) peut suivre un objet en mouvement devant ses yeux, qu’il peut passer d’un objet à un autre avec son regard… 
+            <Image width={16} height={16} src={arrow}/> En bilan, l’ergothérapeute va, par exemple, vérifier que l’enfant (selon l’âge) peut suivre un objet en mouvement devant ses yeux, qu’il peut passer d’un objet à un autre avec son regard… 
             <ul>
                 <li>L’aspect visuo-perceptif « Quoi ? »</li>
             </ul>
             Le visuo-perceptif est un processus d’analyse et de synthèse d’informations visuelles qui permet la reconnaissance des objets, des formes, des couleurs et des tailles.
-            ->En bilan, l’ergothérapeute va , par exemple, demander à l’enfant  (selon l’âge) de reconnaître des formes et de les désigner, de retrouver des formes géométriques parmi plusieurs figures enchevêtrées …
+            <Image width={16} height={16} src={arrow}/> En bilan, l’ergothérapeute va , par exemple, demander à l’enfant  (selon l’âge) de reconnaître des formes et de les désigner, de retrouver des formes géométriques parmi plusieurs figures enchevêtrées …
             <ul>
                 <li>L’aspect visuo-spatial « Où ? » </li>
             </ul>
             La composante visuo-spatiale permet le traitement de l’orientation visuelle ou l’emplacement des objets dans l’espace.
             Ainsi, grâce à cette compétence, nous pouvons situer les objets par rapport à des repères dans l’espace. Exemple : si deux objets sont devant nous, nous sommes capables de savoir quel est le plus près et le plus éloigné de nous.<br/>
             <br/>
-            ->En bilan, l’ergothérapeute va, par exemple, demander à l’enfant (selon l’âge) de situer des éléments visuels les uns par rapport aux autres, vérifier les notions de rotation mentale … <br/>
+            <Image width={16} height={16} src={arrow}/> En bilan, l’ergothérapeute va, par exemple, demander à l’enfant (selon l’âge) de situer des éléments visuels les uns par rapport aux autres, vérifier les notions de rotation mentale … <br/>
             <br/>
             2{")"}	Certains items plutôt moteurs :<br/>
             <br/>
             Le visuo-moteur consiste à organiser les informations visuelles en mouvements adaptés. Exemple : quand nous surlignons un groupe de mot dans un texte avec un surligneur.  <br/>
             <br/>
-            -> En bilan, l’ergothérapeute va, par exemple, demander à l’enfant (selon l’âge) de réaliser un tracé sans dépasser avec un stylo, d’enfiler une perle …
+            <Image width={16} height={16} src={arrow}/> En bilan, l’ergothérapeute va, par exemple, demander à l’enfant (selon l’âge) de réaliser un tracé sans dépasser avec un stylo, d’enfiler une perle …
             <ul>
                 <li>L’aspect visuo-constructif « Je peux construire »</li>
             </ul>
             Le visuo-constructif consiste à intégrer un ensemble d’éléments perçus visuellement en un tout cohérent par le biais d’une réponse motrice. Par exemple : les capacités à dessiner spontanément ou à partir d’un modèle, à construite avec des pièces de bois ou des cubes, à réaliser des puzzles.<br/>
             <br/>
-            -> En bilan, l’ergothérapeute va, par exemple, demander à l’enfant (selon l’âge), de recopier une figure complexe depuis un modèle, de construire des figures avec des cubes … <br/>
+            <Image width={16} height={16} src={arrow}/> En bilan, l’ergothérapeute va, par exemple, demander à l’enfant (selon l’âge), de recopier une figure complexe depuis un modèle, de construire des figures avec des cubes … <br/>
             <br/>
             Quels indices pour les familles ? <br/>
+            <ul>
+                <li>L’enfant a du mal à fixer ou à suivre des yeux quelqu’un ou quelque chose qui se déplace.</li>
+                <li>L’enfant a du mal à trouver ses affaires alors qu’elles se trouvent à un endroit « logique et visible». </li>
+                <li>Difficultés de lecture</li>
+                <li>Difficultés d’orthographe </li>
+                <li>Difficultés graphiques (écriture et géométrie) : écrire sur la ligne, recopier d’un plan à un autre … </li>
+                <li>Difficultés dans les calculs : chiffres mal alignés, chiffres écrits à l’envers… </li>
+                <li>...</li>
+            </ul>
+
+            Que faire ?<br/>
             <br/>
-•	L’enfant a du mal à fixer ou à suivre des yeux quelqu’un ou quelque chose qui se déplace.
-•	L’enfant a du mal à trouver ses affaires alors qu’elles se trouvent à un endroit « logique et visible». 
-•	Difficultés de lecture
-•	Difficultés d’orthographe 
-•	Difficultés graphiques (écriture et géométrie) : écrire sur la ligne, recopier d’un plan à un autre … 
-•	Difficultés dans les calculs : chiffres mal alignés, chiffres écrits à l’envers… 
-•	… 
-Que faire ? 
-Si vous avez l’impression de repérer des difficultés chez votre enfant, parlez-en à votre médecin. Le médecin orientera vers le professionnel de santé le plus adapté aux difficultés de votre enfant. 
-
-Sources : 
-Rauch et Savage (1997) ; (Jeannerod, 1994, p. 217). ; Boller et al. (1984) Benton (1979) ; (Chokron & Marendaz, 2005). Milner & Goodale, 2008). (Ungerleider & Mishkin, 1982).
-
-
+            Si vous avez l’impression de repérer des difficultés chez votre enfant, parlez-en à votre médecin. Le médecin orientera vers le professionnel de santé le plus adapté aux difficultés de votre enfant.<br/>
+            <br/>
+            <div className='note'>
+                Sources :<br/>
+                {"Rauch et Savage (1997) ; (Jeannerod, 1994, p. 217). ; Boller et al. (1984) Benton (1979) ; (Chokron & Marendaz, 2005). Milner & Goodale, 2008). (Ungerleider & Mishkin, 1982)."}
+            </div>
+            
             </Section>
         </Body>
     )
