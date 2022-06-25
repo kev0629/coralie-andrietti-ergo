319cdaf35bbf68ed9d2a1890ef52391b37d1a0d1
kevin <kevin.saffioti@gmail.com>
2022-06-25 09:59:16 +0200

    fix adress in presentation component.

--- component/Presentation.jsx
+++ component/Presentation.jsx
@@ -34,8 +34,8 @@ export default function Presentation(props){
                     <div>En cabinet ou à domicile</div>
                 </div>
                 <div style={{marginBottom:"25px",marginTop:"25px"}}>
-                    <div>Résidence l’Estérel</div>
-                    <div> Bat A 100 chemin de l’Olivet</div>
+                    <div>Résidence Le Parc Victoria</div>
+                    <div> Bat B, 5 chemin de Garibondy</div>
                     <div>06110 Le Cannet </div>
                 </div>
                 <div style={{marginBottom:"25px",marginTop:"25px"}}>coralie.andrietti@gmail.com 06.64.43.62.60</div>
