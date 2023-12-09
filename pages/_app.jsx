a4964f4def5af4f9cc6aaa90bb12787319344a3b
kevin <kevin.saffioti@gmail.com>
2023-12-09 21:01:34 +0100

    lint components and update to Next 14

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,21 @@
-import '../styles/globals.css'
-import '../styles/style_modal.css'
-import React, { useState } from "react";
+import "../styles/globals.css";
+import "../styles/style_modal.css";
+import React from "react";
 import AppContext from "../component/AppContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const [mainModalIsOpen, setMainModalIsOpen] = React.useState(true);
   return (
-    <AppContext.Provider value={{ mainModalIsOpen, setMainModalIsOpen }}>
-    <Component {...pageProps} />
-    </AppContext.Provider>
-    )
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <AppContext.Provider value={{ mainModalIsOpen, setMainModalIsOpen }}>
+        <Component {...pageProps} />
+      </AppContext.Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
