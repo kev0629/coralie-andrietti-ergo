0ea00cbd41433adcad6d82c5f81aef5b71f786d5
Kevin Saffioti <kevin.saffioti@gmail.com>
2025-10-11 16:19:02 +0200

    fix contact button

--- components/sections/Presentation.tsx
+++ components/sections/Presentation.tsx
@@ -7,8 +7,8 @@ interface PresentationProps {
 
 export default function Presentation({ openModalPres }: PresentationProps) {
   return (
-    <div className="flex flex-col justify-center items-center h-full">
-      <div className="relative w-full max-w-lg h-auto aspect-square mt-20">
+    <div className="flex flex-col items-center h-screen">
+      <div className="relative w-full max-w-lg aspect-square mt-20">
         <Image
           src="/pictures/logo-coralie-andrietti-ergo-accueil.svg"
           alt="Logo Coralie Andrietti"
@@ -19,7 +19,7 @@ export default function Presentation({ openModalPres }: PresentationProps) {
       </div>
       <button
         onClick={openModalPres}
-        className="mt-28 bg-gray-500 bg-opacity-50 text-white font-bold py-6 px-16 text-2xl rounded-full shadow-xl hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition-all transform hover:scale-110"
+        className="mt-10 mb-10 bg-gray-500 bg-opacity-50 text-white font-bold py-6 px-16 text-2xl rounded-full shadow-xl hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75 transition-all transform hover:scale-110"
       >
         Prendre contact
       </button>
